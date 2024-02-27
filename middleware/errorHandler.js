df5c316d5c482355095e2a00172dee2d8c853f40
Khageswar Behera <120652613+iamgitkhageswar@users.noreply.github.com>
2024-02-27 16:33:05 +0530

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,28 +1,28 @@
-const {constants}=require("./../constant")
+const {constant}=require("./../constant")
 const errorHandler=(error,req,res,next)=>{
     const statusCode=res.statusCode ? res.statusCode :500
     switch (statusCode) {
-        case constants.VALIDATION_FAILED:
+        case constant.VALIDATION_FAILED:
             res.json({title:'validation_failed',
                 message:err.message,
                 StractTrace:err.stack
             });
             break;
             
-        case constants.UNAUTH0RIZED:
+        case constant.UNAUTH0RIZED:
             res.json({title:'unauthorized_access',
                 message:err.message,
                 StractTrace:err.stack
             })
             
-        case constants.FORBIDDEN:
+        case constant.FORBIDDEN:
             res.json({tittle:'forbidden',
             message:err.message,
             StractTrace:err.stack
                 
            })
            
-           case constants.NOT_FOUND:
+           case constant.NOT_FOUND:
             res.json({tittle:'not_found',
             message:err.message,
             StractTrace:err.stack
